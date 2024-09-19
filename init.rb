134ddb3dcb46769e6567f141e27ca2dcbce01b5f
Maglan Diemer <maglancd@gmail.com>
2024-09-19 17:57:42 -0300

    modified:   init.rb

--- init.rb
+++ init.rb
@@ -1,9 +1,8 @@
-require_relative 'lib/task_copy_hook'
+require_relative "lib/task_copy_hook"
 
-
-Redmine::Plugin.register :sky_redmine do
-  name 'Sky Redmine plugin'
-  author 'Maglan Diemer'
-  description 'Disponibiliza facilitadores para gerar as tarefas de retorno de teste do QS'
-  version '2024.09.17.1'
+Redmine::Plugin.register :sky_redmine_plugin do
+  name "Sky Redmine plugin"
+  author "Maglan Diemer"
+  description "Disponibiliza facilitadores para gerar as tarefas de retorno de teste do QS"
+  version "2024.09.17.1"
 end
