e87f115ef9ea3a4791e7949c8e513257e55ac5ca
Maglan Diemer <maglancd@gmail.com>
2024-09-20 12:59:32 -0300

    nova rota criada

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 post "criar_retorno_testes_qs/:id", to: "criar_retorno_testes#criar_retorno_testes_qs", as: "criar_retorno_testes_qs"
-#post "criar_retorno_testes_devel/:id", to: "criar_retorno_testes#criar_retorno_testes_devel", as: "criar_retorno_testes_devel"
+post "criar_retorno_testes_devel/:id", to: "criar_retorno_testes#criar_retorno_testes_devel", as: "criar_retorno_testes_devel"
