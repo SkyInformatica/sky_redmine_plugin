4a2a8aeb2b0505d56d67907a00b03ba765a0a8e8
Maglan Diemer <maglancd@gmail.com>
2024-09-20 12:43:16 -0300

    ajuste do nome da rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-post "criar_retorno_testes/:id", to: "criar_retorno_testes#criar_retorno_testes_qs", as: "criar_retorno_testes_qs"
+post "criar_retorno_testes_qs/:id", to: "criar_retorno_testes#criar_retorno_testes_qs", as: "criar_retorno_testes_qs"
