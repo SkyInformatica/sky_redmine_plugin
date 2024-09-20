8f394ee9694b9bb0e423c1d327b19d64872fca91
Maglan Diemer <maglancd@gmail.com>
2024-09-20 09:34:10 -0300

    corrigido o nome do arquivo Button para button

--- lib/criar_retorno_testes_hook.rb
+++ lib/criar_retorno_testes_hook.rb
@@ -1,4 +1,4 @@
 class CriarRetornoTestesHook < Redmine::Hook::ViewListener
   # Esse hook insere um botão ou link na parte de baixo da página de uma tarefa
-  render_on :view_issues_show_details_bottom, partial: "issues/criar_retorno_testes_Button"
+  render_on :view_issues_show_details_bottom, partial: "issues/criar_retorno_testes_button"
 end
