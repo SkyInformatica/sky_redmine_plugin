e789eccdd9812f9485053809c9e4718ed945db2e
Maglan Diemer <maglancd@gmail.com>
2024-09-20 12:56:36 -0300

    tentando encontar o erro depois que criei a novo metodo criar_retorno_testes_devel

--- app/controllers/criar_retorno_testes_controller.rb
+++ app/controllers/criar_retorno_testes_controller.rb
@@ -1,10 +1,10 @@
 class CriarRetornoTestesController < ApplicationController
-  before_action :obter_tarefa, only: [:criar_retorno_testes_qs, criar_retorno_testes_devel]
+  before_action :obter_tarefa, only: [:criar_retorno_testes_qs]
 
-  def criar_retorno_testes_devel
-    # Código existente do método
-    flash[:notice] = "criar_retorno_testes_devel foi executado"
-  end
+  #def criar_retorno_testes_devel
+  #  # Código existente do método
+  #  flash[:notice] = "criar_retorno_testes_devel foi executado"
+  #end
 
   def criar_retorno_testes_qs
     allowed_projects = ["Notarial - QS", "Registral - QS"]
