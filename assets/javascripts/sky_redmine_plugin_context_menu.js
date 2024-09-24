d8d2d5bda5734e13f9a53f5fc1a5ef603c56c581
Maglan Diemer <maglancd@gmail.com>
2024-09-24 19:11:16 -0300

    .

--- assets/javascripts/sky_redmine_plugin_context_menu.js
+++ assets/javascripts/sky_redmine_plugin_context_menu.js
@@ -1,44 +1,45 @@
 (function () {
     // Certifique-se que a função de criação de menus de contexto já está definida
     //console.log('inicio sky_redmine_plugin_context_menu')
-    if (typeof contextMenuRightClick !== "undefined") {
-        const originalBuildContextMenu = contextMenuRightClick.prototype.buildContextMenu;
 
-        contextMenuRightClick.prototype.buildContextMenu = function (event) {
-            const menu = originalBuildContextMenu.call(this, event); // Chama o original
+    const originalBuildContextMenu = contextMenuRightClick.prototype.buildContextMenu;
 
-            // Verifique se uma tarefa única está selecionada
-            if (this.getSelection().length === 1) {
-                const issue = this.getSelection()[0];
+    contextMenuRightClick.prototype.buildContextMenu = function (event) {
+        const menu = originalBuildContextMenu.call(this, event); // Chama o original
 
-                const issueStatus = issue.querySelector('.status').textContent.trim();
-                const projectName = issue.querySelector('.project').textContent.trim();
-                const issueId = issue.getAttribute('data-issue-id');
-                //console.log("issueId: ", issueId)
+        // Verifique se uma tarefa única está selecionada
+        if (this.getSelection().length === 1) {
+            const issue = this.getSelection()[0];
 
-                /*
-                // Verifique as condições para adicionar o menu
-                if (issueStatus === "Teste NOK" && ["Notarial - QS", "Registral - QS"].includes(projectName)) {
-                    menu.push({
-                        title: 'Criar tarefa de retorno de testes para essa tarefa de testes',
-                        callback: function() {
-                            $.post(criar_retorno_testes_qs_path({ id: issueId }))
-                                .done(function() { location.reload(); });
-                        }
-                    });
-                } else if (issueStatus === "Resolvida" && !["Notarial - QS", "Registral - QS"].includes(projectName)) {
-                    menu.push({
-                        title: 'Criar tarefa de retorno de testes para essa tarefa de desenvolvimento',
-                        callback: function() {
-                            $.post(criar_retorno_testes_devel_path({ id: issueId }))
-                                .done(function() { location.reload(); });
-                        }
-                    });
-                }
-                */
+            const issueStatus = issue.querySelector('.status').textContent.trim();
+            const projectName = issue.querySelector('.project').textContent.trim();
+            const issueId = issue.getAttribute('data-issue-id');
+            //console.log("issueId: ", issueId)
+
+            /*
+            // Verifique as condições para adicionar o menu
+            if (issueStatus === "Teste NOK" && ["Notarial - QS", "Registral - QS"].includes(projectName)) {
+                menu.push({
+                    title: 'Criar tarefa de retorno de testes para essa tarefa de testes',
+                    callback: function() {
+                        $.post(criar_retorno_testes_qs_path({ id: issueId }))
+                            .done(function() { location.reload(); });
+                    }
+                });
+            } else if (issueStatus === "Resolvida" && !["Notarial - QS", "Registral - QS"].includes(projectName)) {
+                menu.push({
+                    title: 'Criar tarefa de retorno de testes para essa tarefa de desenvolvimento',
+                    callback: function() {
+                        $.post(criar_retorno_testes_devel_path({ id: issueId }))
+                            .done(function() { location.reload(); });
+                    }
+                });
             }
+            */
+        }
 
-            return menu; // Retorna o menu atualizado
-        };
-    }
+        return menu; // Retorna o menu atualizado
+    };
+
 })();
+
