4fe349343b6ac5bb2390df8e54893804b099b084
Maglan Diemer <maglancd@gmail.com>
2024-09-24 15:38:25 -0300

    Ajustes da url do github no init.rb

--- init.rb
+++ init.rb
@@ -3,6 +3,8 @@ require_relative "lib/criar_retorno_testes_hook"
 Redmine::Plugin.register :sky_redmine_plugin do
   name "Sky Redmine plugin"
   author "Maglan Diemer"
-  description "Disponibiliza facilitadores para gerir as tarefas do QS. Veja a [documentação do plugin no GitHub](https://github.com/maglancd/sky-redmine-plugin)."
+  description "Disponibiliza facilitadores para gerir as tarefas do QS."
+  url "https://github.com/maglancd/sky-redmine-plugin"
+  author_url "mailto:maglan.diemer@skyinformatica.com.br"
   version "2024.09.24.1"
 end
