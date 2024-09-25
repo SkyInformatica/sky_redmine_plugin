d30629fb09b170dfd13f0a1f00956ae320ba252d
Maglan Diemer <maglancd@gmail.com>
2024-09-25 16:24:23 -0300

    .

--- lib/sky_redmine_plugin/hooks/criar_retorno_testes_hook.rb
+++ lib/sky_redmine_plugin/hooks/criar_retorno_testes_hook.rb
@@ -1,9 +1,7 @@
-module SkyRedminePlugin
-  class CriarRetornoTestesHook < Redmine::Hook::ViewListener
-    # Esse hook insere um botão ou link na parte de baixo da página de uma tarefa
-    render_on :view_issues_show_details_bottom, partial: "issues/criar_retorno_testes_button"
+class CriarRetornoTestesHook < Redmine::Hook::ViewListener
+  # Esse hook insere um botão ou link na parte de baixo da página de uma tarefa
+  render_on :view_issues_show_details_bottom, partial: "issues/criar_retorno_testes_button"
 
-    # Este hook adiciona itens ao menu de contexto
-    #render_on :view_issues_context_menu_start, partial: "issues/criar_retorno_testes_context_menu"
-  end
+  # Este hook adiciona itens ao menu de contexto
+  #render_on :view_issues_context_menu_start, partial: "issues/criar_retorno_testes_context_menu"
 end
