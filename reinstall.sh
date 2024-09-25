f4a7618dd8efb049ab02d5f4e82f8748876e4c40
Maglan Diemer <maglancd@gmail.com>
2024-09-25 16:30:17 -0300

    ajustesreinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 service apache2 stop
-echo "" > /opt/redmine/production.log
+echo "" > /opt/redmine/log/production.log
 bundle exec rake redmine:plugins:migrate NAME=sky_redmine_plugin VERSION=0 RAILS_ENV=production
 git pull origin main
 bundle exec rake redmine:plugins:migrate RAILS_ENV=production
