87fbeaf8e5e7eeaac1ee9b09b05a570940e30a74
Maglan Diemer <maglancd@gmail.com>
2024-09-27 14:55:11 -0300

    ajustes nas classes do hook

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
+
+# retorno de testes
 post "criar_retorno_testes_qs/:id", to: "criar_retorno_testes#criar_retorno_testes_qs", as: "criar_retorno_testes_qs"
 post "criar_retorno_testes_devel/:id", to: "criar_retorno_testes#criar_retorno_testes_devel", as: "criar_retorno_testes_devel"
-
 get "criar_retorno_testes_lote", to: "criar_retorno_testes#criar_retorno_testes_lote", as: "criar_retorno_testes_lote"
+
+# continua na proxima sprint
+#get "continua_na_proxima_sprint_lote", to: "continua_na_proxima_sprint#continua_na_proxima_sprint_lote", as: "continua_na_proxima_sprint_lote"
--- lib/sky_redmine_plugin/hooks/criar_retorno_testes_view_issues_hook.rb
+++ lib/sky_redmine_plugin/hooks/criar_retorno_testes_view_issues_hook.rb
@@ -4,5 +4,10 @@ module SkyRedminePlugin
       # Esse hook insere um botão ou link na parte de baixo da página de uma tarefa
       render_on :view_issues_show_details_bottom, partial: "issues/criar_retorno_testes_issues"
     end
+
+    class CriarRetornoTestesContextMenuHook < Redmine::Hook::ViewListener
+      # Este hook adiciona itens ao menu de contexto
+      render_on :view_issues_context_menu_start, partial: "context_menu/criar_retorno_testes_context_menu"
+    end
   end
 end
