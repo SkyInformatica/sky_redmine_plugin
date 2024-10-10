0215bb6ddf2dbf2398224539ad0d8b6626f5bab9
Maglan Diemer <maglancd@gmail.com>
2024-10-10 17:42:27 -0300

    .

--- lib/sky_redmine_plugin/hooks/retorno_testes_context_menu_hook.rb
+++ lib/sky_redmine_plugin/hooks/retorno_testes_context_menu_hook.rb
@@ -3,7 +3,7 @@ module SkyRedminePlugin
     class RetornoTestesContextMenuHook < Redmine::Hook::ViewListener
       # retorno de testes
 
-      render_on :view_issues_context_menu_start, partial: "context_menu/retorno_testes_context_menu"
+      #render_on :view_issues_context_menu_start, partial: "context_menu/retorno_testes_context_menu"
     end
   end
 end
