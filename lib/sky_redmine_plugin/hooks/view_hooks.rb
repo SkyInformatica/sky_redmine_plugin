fa562d0499f3509fe5c363d0544e1d5c3d084e3b
Maglan Diemer <maglancd@gmail.com>
2024-10-10 17:36:05 -0300

    .

--- lib/sky_redmine_plugin/hooks/view_hooks.rb
+++ lib/sky_redmine_plugin/hooks/view_hooks.rb
@@ -1,12 +1,11 @@
 module SkyRedminePlugin
   module Hooks
     class ViewHooks < Redmine::Hook::ViewListener
-
       # retorno de testes
       render_on :view_issues_show_details_bottom, partial: "issues/retorno_testes_issues"
       render_on :view_issues_context_menu_start, partial: "context_menu/retorno_testes_context_menu"
 
-      # encaminha para QS
+      # encaminhar para QS
       render_on :view_issues_context_menu_start, partial: "context_menu/encaminhar_qs_context_menu"
 
       # continua na proxima sprint
