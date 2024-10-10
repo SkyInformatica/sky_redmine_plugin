c8268d84e7b919b07b212524a8d13ede37ac987c
Maglan Diemer <maglancd@gmail.com>
2024-10-10 17:44:54 -0300

    .

--- lib/sky_redmine_plugin/hooks/view_hooks.rb
+++ lib/sky_redmine_plugin/hooks/view_hooks.rb
@@ -6,7 +6,7 @@ module SkyRedminePlugin
       render_on :view_issues_context_menu_start, partial: "context_menu/retorno_testes_context_menu"
 
       # encaminhar para QS
-      render_on :view_issues_context_menu_start, partial: "context_menu/encaminhar_qs_context_menu"
+      #render_on :view_issues_context_menu_start, partial: "context_menu/encaminhar_qs_context_menu"
 
       # continua na proxima sprint
       #render_on :view_issues_context_menu_start, partial: "context_menu/continua_proxima_sprint_context_menu"
