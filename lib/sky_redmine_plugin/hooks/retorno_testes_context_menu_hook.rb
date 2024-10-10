c53e07fc859ec936bfd13387893aceea62a393c8
Maglan Diemer <maglancd@gmail.com>
2024-10-10 17:39:39 -0300

    .

---
+++ lib/sky_redmine_plugin/hooks/retorno_testes_context_menu_hook.rb
@@ -0,0 +1,9 @@
+module SkyRedminePlugin
+  module Hooks
+    class RetornoTetesContextMenuHook < Redmine::Hook::ViewListener
+      # retorno de testes
+
+      render_on :view_issues_context_menu_start, partial: "context_menu/retorno_testes_context_menu"
+    end
+  end
+end
--- lib/sky_redmine_plugin/hooks/view_hooks.rb
+++ lib/sky_redmine_plugin/hooks/view_hooks.rb
@@ -3,13 +3,13 @@ module SkyRedminePlugin
     class ViewHooks < Redmine::Hook::ViewListener
       # retorno de testes
       render_on :view_issues_show_details_bottom, partial: "issues/retorno_testes_issues"
-      render_on :view_issues_context_menu_start, partial: "context_menu/retorno_testes_context_menu"
+      #render_on :view_issues_context_menu_start, partial: "context_menu/retorno_testes_context_menu"
 
       # encaminhar para QS
-      render_on :view_issues_context_menu_start, partial: "context_menu/encaminhar_qs_context_menu"
+      #render_on :view_issues_context_menu_start, partial: "context_menu/encaminhar_qs_context_menu"
 
       # continua na proxima sprint
-      render_on :view_issues_context_menu_start, partial: "context_menu/continua_proxima_sprint_context_menu"
+      #render_on :view_issues_context_menu_start, partial: "context_menu/continua_proxima_sprint_context_menu"
     end
   end
 end
