bed435bc2813d1d341829173f9a8865dfbeb0b5b
Maglan Diemer <maglancd@gmail.com>
2024-10-10 17:50:55 -0300

    ajuste para todos os menus de contexto no mesmo arquivo .html.erb

--- lib/sky_redmine_plugin/hooks/view_hooks.rb
+++ lib/sky_redmine_plugin/hooks/view_hooks.rb
@@ -3,7 +3,7 @@ module SkyRedminePlugin
     class ViewHooks < Redmine::Hook::ViewListener
       # retorno de testes
       render_on :view_issues_show_details_bottom, partial: "issues/retorno_testes_issues"
-      render_on :view_issues_context_menu_start, partial: "context_menu/retorno_testes_context_menu"
+      render_on :view_issues_context_menu_start, partial: "context_menu/criar_tarefas"
 
       # encaminhar para QS
       #render_on :view_issues_context_menu_start, partial: "context_menu/encaminhar_qs_context_menu"
