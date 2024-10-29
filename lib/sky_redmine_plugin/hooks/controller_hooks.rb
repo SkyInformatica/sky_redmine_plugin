d267b8b4544db6679cb4f5b66b73e4fd673c9e6d
Maglan Diemer <maglancd@gmail.com>
2024-10-29 10:44:19 -0300

    .

--- lib/sky_redmine_plugin/hooks/controller_hooks.rb
+++ lib/sky_redmine_plugin/hooks/controller_hooks.rb
@@ -11,11 +11,11 @@ module SkyRedminePlugin
           Rails.logger.info ">>> definindo a data de inicio da tarefa #{issue.id}"
           # Adiciona uma nota ao journal existente
           if journal
-            journal.notes = (journal.notes || "") + "\nData de início definida pelo SkyRedminePlugin."
+            journal.notes = (journal.notes || "") + "\nData de início definida pelo SkyRedminePlugin (journal)"
             journal.save
           else
             # Se não houver um journal, inicializa um novo
-            issue.init_journal(User.current, "Data de início definida pelo SkyRedminePlugin.")
+            issue.init_journal(User.current, "Data de início definida pelo SkyRedminePlugin (init_journal)")
           end
           issue.start_date = Date.today
           issue.save(validate: false)
