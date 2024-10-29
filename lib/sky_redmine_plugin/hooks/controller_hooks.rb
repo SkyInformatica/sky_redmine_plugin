9d642fd3f328c1879b673f7645ddb102c90df8a4
Maglan Diemer <maglancd@gmail.com>
2024-10-29 09:22:52 -0300

    .

--- lib/sky_redmine_plugin/hooks/controller_hooks.rb
+++ lib/sky_redmine_plugin/hooks/controller_hooks.rb
@@ -2,11 +2,13 @@ module SkyRedminePlugin
   module Hooks
     class ControllerHooks < Redmine::Hook::Listener
       def controller_issues_edit_after_save(context = {})
+        Rails.logger.info ">>> controller_issues_edit_after_save"
         issue = context[:issue]
         journal = context[:journal]
 
         # Verifica se o status mudou para 'Em Andamento' e a data de início não está definida
         if issue.status.name == "Em Andamento" && issue.start_date.nil?
+          Rails.logger.info ">>> definindo a data de inicio da tarefa #{issue.id}"
           issue.init_journal(User.current, "Data de início atualizada automaticamente.")
           issue.start_date = Date.today
           issue.save(validate: false)
