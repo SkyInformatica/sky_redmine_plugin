e93a53d8ff149c45634093f98334155088a3e61d
Maglan Diemer <maglancd@gmail.com>
2024-10-29 10:42:26 -0300

    Ajustado mensagem do evento edit_after_save

--- lib/sky_redmine_plugin/hooks/controller_hooks.rb
+++ lib/sky_redmine_plugin/hooks/controller_hooks.rb
@@ -9,7 +9,14 @@ module SkyRedminePlugin
         # Verifica se o status mudou para 'Em Andamento' e a data de início não está definida
         if issue.status.name == SkyRedminePlugin::Constants::IssueStatus::EM_ANDAMENTO && issue.start_date.nil?
           Rails.logger.info ">>> definindo a data de inicio da tarefa #{issue.id}"
-          issue.init_journal(User.current, "Data de início atualizada automaticamente.")
+          # Adiciona uma nota ao journal existente
+          if journal
+            journal.notes = (journal.notes || "") + "\nData de início definida pelo SkyRedminePlugin."
+            journal.save
+          else
+            # Se não houver um journal, inicializa um novo
+            issue.init_journal(User.current, "Data de início definida pelo SkyRedminePlugin.")
+          end
           issue.start_date = Date.today
           issue.save(validate: false)
         end
