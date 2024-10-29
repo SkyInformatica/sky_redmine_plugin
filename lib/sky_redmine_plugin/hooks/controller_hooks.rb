482c1c2e14d744045840584019cd867752ca6686
Maglan Diemer <maglancd@gmail.com>
2024-10-29 19:27:10 -0300

    removido o debug

--- lib/sky_redmine_plugin/hooks/controller_hooks.rb
+++ lib/sky_redmine_plugin/hooks/controller_hooks.rb
@@ -40,14 +40,10 @@ module SkyRedminePlugin
       private
 
       def atualizar_status_tarefa_qs_tarefa_devel(issue, new_status_name)
-        Rails.logger.info ">>> controller_issues_edit_after_save"
         if SkyRedminePlugin::Constants::Projects::QS_PROJECTS.include?(issue.project.name)
-          Rails.logger.info ">>> tarefa projeto #{issue.project.name} atualizando status para #{new_status_name}"
           devel_issue = localizar_tarefa_origem_desenvolvimento(issue)
           if devel_issue
-            Rails.logger.info ">>> tarefa devel associada é #{devel_issue.id}"
             if custom_field = IssueCustomField.find_by(name: SkyRedminePlugin::Constants::CustomFields::TESTE_QS)
-              Rails.logger.info ">>> atualizando campo Teste QS da tarefa #{devel_issue.id} para #{new_status_name}"
               devel_issue.custom_field_values = { custom_field.id => new_status_name }
               devel_issue.save
             end
