fa01041179f90641facdcd80c8a53f54a000f5dc
Maglan Diemer <maglancd@gmail.com>
2024-11-01 14:31:48 -0300

    ajuste init.rb para as dependencias

--- init.rb
+++ init.rb
@@ -6,7 +6,8 @@ Redmine::Plugin.register :sky_redmine_plugin do
   description "Disponibiliza facilitadores para gerir as tarefas do QS."
   url "https://github.com/SkyInformatica/sky_redmine_plugin"
   author_url "mailto:maglan.diemer@skyinformatica.com.br"
-  version "2024.10.30.1"
+  version "2024.11.01.1"
 end
 
 require File.dirname(__FILE__) + "/lib/sky_redmine_plugin"
+require File.dirname(__FILE__) + "/app/helpers"
