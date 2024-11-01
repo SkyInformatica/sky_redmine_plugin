3b9d8b7e4c24b9642e131fc936b076517f8f42ae
Maglan Diemer <maglancd@gmail.com>
2024-11-01 14:41:00 -0300

    ajuste no reinstall.sh para reiniciar o arquivo de log de erros do apache

--- reinstall.sh
+++ reinstall.sh
@@ -2,6 +2,7 @@
 
 service apache2 stop
 echo "" > /opt/redmine/log/production.log
+echo "" > /var/log/apache2/error.log
 bundle exec rake redmine:plugins:migrate NAME=sky_redmine_plugin VERSION=0 RAILS_ENV=production
 git pull origin main
 bundle exec rake redmine:plugins:migrate RAILS_ENV=production
