2f769d5bae4b81038e8a728f0f6dce8d17ae2922
Maglan Diemer <maglancd@gmail.com>
2024-11-01 11:38:45 -0300

    removido tags URGENTE e PRIORIDADE nas tags automatizadas

--- lib/sky_redmine_plugin/hooks/view_hooks.rb
+++ lib/sky_redmine_plugin/hooks/view_hooks.rb
@@ -1,16 +1,9 @@
-# ./lib/sky_redmine_plugin/hoogs/view_hooks.rb
+# ./lib/sky_redmine_plugin/hooks/view_hooks.rb
 module SkyRedminePlugin
   module Hooks
     class ViewHooks < Redmine::Hook::ViewListener
-      # retorno de testes
       render_on :view_issues_show_details_bottom, partial: "issues/criar_tarefas"
       render_on :view_issues_context_menu_start, partial: "context_menu/criar_tarefas"
-
-      # encaminhar para QS
-      #render_on :view_issues_context_menu_start, partial: "context_menu/encaminhar_qs_context_menu"
-
-      # continua na proxima sprint
-      #render_on :view_issues_context_menu_start, partial: "context_menu/continua_proxima_sprint_context_menu"
     end
   end
 end
