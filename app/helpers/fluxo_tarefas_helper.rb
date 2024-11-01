5bf8f1ca85dab19f428d686621502c50a5e082ba
Maglan Diemer <maglancd@gmail.com>
2024-11-01 14:58:37 -0300

    .

--- app/helpers/fluxo_tarefas_helper.rb
+++ app/helpers/fluxo_tarefas_helper.rb
@@ -227,14 +227,14 @@ module FluxoTarefasHelper
 
     # Gerar o texto final
     linhas = []
-    linhas << "<h2>Fluxo das tarefas<br></h2>"
+    linhas << "<h4>Fluxo das tarefas<br></h4>"
     secoes.each do |secao|
       # Calcular tempo total gasto na seção
       total_tempo = secao[:tarefas].sum { |t| t.spent_hours.to_f }
       total_tempo_formatado = format("%.2f", total_tempo)
 
       # Adicionar cabeçalho da seção com tempo total
-      linhas << "<h2>#{secao[:nome]} (Tempo gasto total: #{total_tempo_formatado}h)</h2>"
+      linhas << "<h4>#{secao[:nome]} (Tempo gasto total: #{total_tempo_formatado}h)</h4>"
       linhas << "<table>"
       linhas << "<tr><th>Nº</th><th>Projeto</th><th>ID</th><th>Status</th><th>Data de Início</th><th>Versão</th><th>Horas Gastas</th></tr>"
 
