de899785b001924427f2ffeab52edcac9900a16a
Maglan Diemer <maglancd@gmail.com>
2024-11-01 16:05:43 -0300

    .

--- app/helpers/fluxo_tarefas_helper.rb
+++ app/helpers/fluxo_tarefas_helper.rb
@@ -301,6 +301,7 @@ module FluxoTarefasHelper
   def formatar_linha_tarefa_html(tarefa, numero_sequencial, tarefa_atual_id)
     horas_gastas = format("%.2f", tarefa.spent_hours.to_f)
     data_previsao = tarefa.due_date || "<previsao>"
+    assigned_to_name = task.assigned_to_id.present? ? User.find(task.assigned_to_id).name : "Não atribuído"
     version_name = tarefa.fixed_version ? link_to(tarefa.fixed_version.name, version_path(tarefa.fixed_version)) : "-"
     link_tarefa = link_to_issue(tarefa)
 
@@ -311,7 +312,7 @@ module FluxoTarefasHelper
     "<tr>        
       <td>#{numero_sequencial} #{tarefa.project.name} - #{link_tarefa}</td>        
       <td>#{tarefa.status.name}</td>  
-      <td>#{tarefa.assigned_to.name}</td>
+      <td>#{assigned_to_name}</td>
       <td>#{data_previsao}</td>  
       <td>#{version_name}</td>  
       <td>#{horas_gastas}h</td>  
