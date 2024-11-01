ff9d2c9e9d071f564c1a00ce59b7e78a4de23d51
Maglan Diemer <maglancd@gmail.com>
2024-11-01 19:03:38 -0300

    .

--- init.rb
+++ init.rb
@@ -9,12 +9,6 @@ Redmine::Plugin.register :sky_redmine_plugin do
   url "https://github.com/SkyInformatica/sky_redmine_plugin"
   author_url "mailto:maglan.diemer@skyinformatica.com.br"
   version "2024.11.01.1"
-
-  assets_directory = File.join(__dir__, "assets")
-  directories = [assets_directory]
-  directories.each do |dir|
-    Redmine::Plugin::registered_plugins[:sky_redmine_plugin].assets_paths << dir
-  end
 end
 
 ActionView::Base.send :include, FluxoTarefasHelper
