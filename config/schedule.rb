0ccec504e56db9ff53e14045cb0afa11b57a3a96
Maglan Diemer <maglancd@gmail.com>
2024-11-03 11:23:40 -0300

    .

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ set :output, File.expand_path("../../log/cron_log.log", __FILE__)
 env :PATH, ENV["PATH"]
 env :GEM_PATH, ENV["GEM_PATH"]
 
-every 1.day, at: "6:00 pm" do
+every 1.day, at: "7:00 pm" do
   rake "bigquery:atualizar"
 end
