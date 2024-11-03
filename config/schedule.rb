2a99d63449c1091117698e7c0bc50dff0a05f658
Maglan Diemer <maglancd@gmail.com>
2024-11-03 11:46:11 -0300

    removido tarefa do google-cloud

--- config/schedule.rb
+++
@@ -1,8 +0,0 @@
-=begin set :output, File.expand_path("../../log/cron_log.log", __FILE__)
-env :PATH, ENV["PATH"]
-env :GEM_PATH, ENV["GEM_PATH"]
-
-every 1.day, at: "7:00 pm" do
-  rake "bigquery:atualizar"
-end
- =end
