5aafb658bee451168a460a5763b42fc5efa2d883
Maglan Diemer <maglancd@gmail.com>
2024-11-28 17:17:44 -0300

    .

--- lib/sky_redmine_plugin/issue_helper_patch.rb
+++ lib/sky_redmine_plugin/issue_helper_patch.rb
@@ -1,29 +1,21 @@
 # lib/sky_redmine_plugin/issue_helper_patch.rb
 module SkyRedminePlugin
   module IssueHelperPatch
-    extend ActiveSupport::Concern
-
-    included do
-      def issue_history_tabs
-        Rails.logger.info ">>>> SkyRedminePlugin: Chamando issue_history_tabs"
-        # Chama o método original para obter as abas existentes
-        tabs = super
-
-        # Adiciona a nova aba ao array de abas
-        tabs << {
-          name: "fluxo_tarefas",
-          partial: "issues/tabs/fluxo_tarefas",
-          label: "Fluxo das tarefas",
-          locals: { issue: @issue },
-        }
-
-        tabs
-      end
+    def issue_history_tabs
+      Rails.logger.info "SkyRedminePlugin: Chamando issue_history_tabs com prepend"
+      tabs = super
+      tabs << {
+        name: "fluxo_tarefas",
+        partial: "issues/tabs/fluxo_tarefas",
+        label: "Fluxo das tarefas",
+        locals: { issue: @issue },
+      }
+      tabs
     end
   end
 end
 
-# Aplica o patch ao IssuesHelper
 Rails.application.config.to_prepare do
-  IssuesHelper.include SkyRedminePlugin::IssueHelperPatch
+  Rails.logger.info "SkyRedminePlugin: Usando prepend para IssueHelperPatch"
+  IssuesHelper.prepend SkyRedminePlugin::IssueHelperPatch
 end
