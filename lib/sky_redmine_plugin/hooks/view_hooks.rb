cb79cb736403fa20cc1336d9201f17c54f7803c5
Maglan Diemer <maglancd@gmail.com>
2024-11-28 11:43:50 -0300

    teste para ajustar o fluxo de tarefas em uma aba junto com o historico

--- lib/sky_redmine_plugin/hooks/view_hooks.rb
+++ lib/sky_redmine_plugin/hooks/view_hooks.rb
@@ -4,7 +4,8 @@ module SkyRedminePlugin
     class ViewHooks < Redmine::Hook::ViewListener
       render_on :view_issues_show_details_bottom, partial: "issues/criar_tarefas"
       render_on :view_issues_context_menu_start, partial: "context_menu/criar_tarefas"
-      render_on :view_issues_show_description_bottom, partial: "issues/fluxo_tarefas"
+      #render_on :view_issues_show_description_bottom, partial: "issues/fluxo_tarefas"
+      render_on :view_journals_notes_form_after_notes, partial: "issues/fluxo_tarefas"
 
       def view_layouts_base_html_head(context = {})
         javascript_include_tag("ocultar_tarefas_relacionadas", plugin: "sky_redmine_plugin")
