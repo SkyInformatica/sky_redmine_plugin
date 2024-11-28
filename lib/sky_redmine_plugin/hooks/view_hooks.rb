4e3d20e3e1f2c7c0bfff59111d02198eac76c877
Maglan Diemer <maglancd@gmail.com>
2024-11-28 16:33:31 -0300

    .

--- lib/sky_redmine_plugin/hooks/view_hooks.rb
+++ lib/sky_redmine_plugin/hooks/view_hooks.rb
@@ -5,14 +5,6 @@ module SkyRedminePlugin
       render_on :view_issues_show_details_bottom, partial: "issues/criar_tarefas"
       render_on :view_issues_context_menu_start, partial: "context_menu/criar_tarefas"
       #render_on :view_issues_show_description_bottom, partial: "issues/fluxo_tarefas"
-      # Adiciona a aba personalizada na visualização da tarefa
-      def helper_issues_show_detail_after_setting(context = {})
-        context[:tabs] << {
-          name: "fluxo_tarefas",
-          partial: "issues/fluxo_tarefas",
-          label: "Fluxo das tarefas",
-        }
-      end
 
       def view_layouts_base_html_head(context = {})
         javascript_include_tag("ocultar_tarefas_relacionadas", plugin: "sky_redmine_plugin")
