5d61e638d37e732e728d98ddb605563e6604174f
Maglan Diemer <maglancd@gmail.com>
2024-11-28 11:57:27 -0300

    .

--- lib/sky_redmine_plugin/hooks/view_hooks.rb
+++ lib/sky_redmine_plugin/hooks/view_hooks.rb
@@ -4,8 +4,16 @@ module SkyRedminePlugin
     class ViewHooks < Redmine::Hook::ViewListener
       render_on :view_issues_show_details_bottom, partial: "issues/criar_tarefas"
       render_on :view_issues_context_menu_start, partial: "context_menu/criar_tarefas"
-      #render_on :view_issues_show_description_bottom, partial: "issues/fluxo_tarefas"
-      render_on :view_journals_notes_form_after_notes, partial: "issues/fluxo_tarefas"
+      render_on :view_issues_show_description_bottom, partial: "issues/fluxo_tarefas"
+
+      def view_issues_show_details_bottom(context = {})
+        # Adicionar uma aba personalizada
+        context[:controller].send(:add_tab, {
+          name: "fluxo_tarefas", # Nome da aba (usado como identificador)
+          partial: "issues/fluxo_tarefas_tab", # Parcial que será renderizada
+          label: "Fluxo das tarefas", # Label para a aba
+        })
+      end
 
       def view_layouts_base_html_head(context = {})
         javascript_include_tag("ocultar_tarefas_relacionadas", plugin: "sky_redmine_plugin")
