6ef98d24e85e431d719832129a1025e88d808801
Maglan Diemer <maglancd@gmail.com>
2024-11-29 10:22:01 -0300

    .

---
+++ assets/javascripts/ocultar_tarefas_relacionadas.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var instrucoesTestesDiv = document.getElementById('relations');
+    if (instrucoesTestesDiv) {
+        instrucoesTestesDiv.style.display = 'none';
+    }
+});
--- lib/sky_redmine_plugin/hooks/view_hooks.rb
+++ lib/sky_redmine_plugin/hooks/view_hooks.rb
@@ -7,7 +7,8 @@ module SkyRedminePlugin
       #render_on :view_issues_show_description_bottom, partial: "issues/tabs/fluxo_tarefas"
 
       def view_layouts_base_html_head(context = {})
-        javascript_include_tag("ocultar_instrucoes_testes", plugin: "sky_redmine_plugin")
+        javascript_include_tag("ocultar_tarefas_relacionadas", plugin: "sky_redmine_plugin") +
+          javascript_include_tag("ocultar_instrucoes_testes", plugin: "sky_redmine_plugin")
       end
     end
   end
