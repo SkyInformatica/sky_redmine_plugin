2f6d5c2a4a37797d683083da3bab6b84964920c6
Maglan Diemer <maglancd@gmail.com>
2024-12-06 15:54:52 -0300

    novas colunas no fluxo das tarefas

--- app/helpers/fluxo_tarefas_helper.rb
+++ app/helpers/fluxo_tarefas_helper.rb
@@ -52,6 +52,17 @@ module FluxoTarefasHelper
     tarefas
   end
 
+  def obter_data_mudanca_status(tarefa, status_nomes)
+    status_ids = IssueStatus.where(name: status_nomes).pluck(:id)
+
+    journal = tarefa.journals.joins(:details)
+                    .where(journal_details: { property: "attr", prop_key: "status_id", value: status_ids })
+                    .order("created_on ASC")
+                    .first
+
+    journal&.created_on
+  end 
+
   def gerar_texto_fluxo_html(tarefas, tarefa_atual_id)
     secoes = []
     secao_atual = nil
@@ -90,50 +101,72 @@ module FluxoTarefasHelper
     linhas << "<div class='description'>"
     linhas << "<hr>"
     linhas << "<p><strong>Fluxo das tarefas</strong></b></p>"
+   
+    =begin 
+      linhas << "<style>  
+                .tabela-fluxo-tarefas {  
+                  border-collapse: collapse;  
+                  table-layout: fixed; /* Definição importante para controlar as larguras */
+                  width: 100%; /* Ocupará toda a largura disponível */  
+                  margin: 0 auto; /* Centraliza a tabela */                  
+                }  
+                .tabela-fluxo-tarefas th,  
+                .tabela-fluxo-tarefas td {  
+                  border: 1px solid #dddddd;  
+                  text-align: left;  
+                  padding: 4px;  
+                  word-wrap: break-word; /* Quebra palavras longas */                
+                }        
+                  .tabela-fluxo-tarefas th:nth-child(1),  
+                  .tabela-fluxo-tarefas td:nth-child(1) {  
+                      width: 50%; 
+                  }  
+
+                  .tabela-fluxo-tarefas th:nth-child(2),  
+                  .tabela-fluxo-tarefas td:nth-child(2) {  
+                      width: 15%; 
+                  }  
+
+                  .tabela-fluxo-tarefas th:nth-child(3),  
+                  .tabela-fluxo-tarefas td:nth-child(3) {  
+                      width: 12%; 
+                  }  
+
+                  .tabela-fluxo-tarefas th:nth-child(4),  
+                  .tabela-fluxo-tarefas td:nth-child(4) {  
+                      width: 6%; 
+                  }  
+
+                  .tabela-fluxo-tarefas th:nth-child(5),  
+                  .tabela-fluxo-tarefas td:nth-child(5) {  
+                      width: 13%; 
+                  }  
+
+                  .tabela-fluxo-tarefas th:nth-child(6),  
+                  .tabela-fluxo-tarefas td:nth-child(6) {  
+                      width: 4%; 
+                  }  
+              </style>"
+    =end
+    
     linhas << "<style>  
-              .tabela-fluxo-tarefas {  
-                border-collapse: collapse;  
-                table-layout: fixed; /* Definição importante para controlar as larguras */
-                width: 100%; /* Ocupará toda a largura disponível */  
-                margin: 0 auto; /* Centraliza a tabela */                  
-              }  
-              .tabela-fluxo-tarefas th,  
-              .tabela-fluxo-tarefas td {  
-                border: 1px solid #dddddd;  
-                text-align: left;  
-                padding: 4px;  
-                word-wrap: break-word; /* Quebra palavras longas */                
-              }        
-                .tabela-fluxo-tarefas th:nth-child(1),  
-                .tabela-fluxo-tarefas td:nth-child(1) {  
-                    width: 50%; 
-                }  
-
-                .tabela-fluxo-tarefas th:nth-child(2),  
-                .tabela-fluxo-tarefas td:nth-child(2) {  
-                    width: 15%; 
-                }  
-
-                .tabela-fluxo-tarefas th:nth-child(3),  
-                .tabela-fluxo-tarefas td:nth-child(3) {  
-                    width: 12%; 
-                }  
-
-                .tabela-fluxo-tarefas th:nth-child(4),  
-                .tabela-fluxo-tarefas td:nth-child(4) {  
-                    width: 6%; 
-                }  
-
-                .tabela-fluxo-tarefas th:nth-child(5),  
-                .tabela-fluxo-tarefas td:nth-child(5) {  
-                    width: 13%; 
-                }  
-
-                .tabela-fluxo-tarefas th:nth-child(6),  
-                .tabela-fluxo-tarefas td:nth-child(6) {  
-                    width: 4%; 
-                }  
-            </style>"
+            .tabela-fluxo-tarefas {  
+              border-collapse: collapse;  
+              table-layout: fixed;  
+              width: 100%;  
+              margin: 0 auto;  
+            }  
+            .tabela-fluxo-tarefas th,  
+            .tabela-fluxo-tarefas td {  
+              border: 1px solid #dddddd;  
+              text-align: left;  
+              padding: 4px;  
+              word-wrap: break-word;  
+            }  
+            .tabela-fluxo-tarefas th, .tabela-fluxo-tarefas td {  
+              width: calc(100% / 9);  
+            }  
+          </style>"
     secoes.each do |secao|
       # Calcular tempo total gasto na seção
       total_tempo = secao[:tarefas].sum { |t| t.spent_hours.to_f }
@@ -143,6 +176,17 @@ module FluxoTarefasHelper
       linhas << "<b>#{secao[:nome]}</b> (Tempo gasto total: #{total_tempo_formatado}h)"
       #linhas << "<table class='tabela-fluxo-tarefas'>"
       linhas << "<table class='tabela-fluxo-tarefas'>"
+      linhas << "<tr>  
+        <th>Título</th>  
+        <th>Situação</th>  
+        <th>Atribuído Para</th>  
+        <th>Data Criação</th>  
+        <th>Data Em Andamento</th>  
+        <th>Data Resolvida/Teste</th>  
+        <th>Data Fechada</th>  
+        <th>Versão</th>  
+        <th>Tempo Gasto</th>  
+      </tr>" 
 
       # Adicionar as tarefas
       secao[:tarefas].each do |tarefa|
@@ -159,7 +203,7 @@ module FluxoTarefasHelper
     linhas.join("\n")
   end
 
-  def formatar_linha_tarefa_html(tarefa, numero_sequencial, tarefa_atual_id)
+  def formatar_linha_tarefa_html_old(tarefa, numero_sequencial, tarefa_atual_id)
     horas_gastas = format("%.2f", tarefa.spent_hours.to_f)
     data_inicio = tarefa.start_date.present? ? tarefa.start_date.strftime("%d/%m/%Y") : ""
     #assigned_to_name = tarefa.assigned_to_id.present? ? User.find(tarefa.assigned_to_id).name : "Não atribuído"
@@ -179,5 +223,59 @@ module FluxoTarefasHelper
       <td class='version'>#{version_name}</td>  
       <td class='spent_hours'>#{horas_gastas}h</td>  
     </tr>"
+  end
+
+  def formatar_linha_tarefa_html(tarefa, numero_sequencial, tarefa_atual_id)  
+    horas_gastas = format("%.2f", tarefa.spent_hours.to_f)  
+    data_criacao = tarefa.created_on.strftime("%d/%m/%Y")  
+  
+    # Obter a data que mudou para "Em andamento"  
+    data_em_andamento = obter_data_mudanca_status(tarefa, [SkyRedminePlugin::Constants::IssueStatus::EM_ANDAMENTO])  
+    data_em_andamento = data_em_andamento.strftime("%d/%m/%Y") if data_em_andamento  
+  
+    projeto_nome = tarefa.project.name  
+    if SkyRedminePlugin::Constants::Projects::QS_PROJECTS.include?(projeto_nome)  
+      # Tarefas do QS  
+      status_resolvida = [  
+        SkyRedminePlugin::Constants::IssueStatus::TESTE_OK,  
+        SkyRedminePlugin::Constants::IssueStatus::TESTE_NOK  
+      ]  
+      status_fechada = [  
+        SkyRedminePlugin::Constants::IssueStatus::TESTE_OK_FECHADA,  
+        SkyRedminePlugin::Constants::IssueStatus::TESTE_NOK_FECHADA  
+      ]  
+    else  
+      # Tarefas de Desenvolvimento  
+      status_resolvida = [SkyRedminePlugin::Constants::IssueStatus::RESOLVIDA]  
+      status_fechada = [SkyRedminePlugin::Constants::IssueStatus::FECHADA]  
+    end  
+  
+    # Obter a data que mudou para "Resolvida" ou "Teste OK"/"Teste NOK"  
+    data_resolvida = obter_data_mudanca_status(tarefa, status_resolvida)  
+    data_resolvida = data_resolvida.strftime("%d/%m/%Y") if data_resolvida  
+  
+    # Obter a data que mudou para "Fechada" ou "Teste OK - Fechada"/"Teste NOK - Fechada"  
+    data_fechada = obter_data_mudanca_status(tarefa, status_fechada)  
+    data_fechada = data_fechada.strftime("%d/%m/%Y") if data_fechada  
+  
+    assigned_to_name = tarefa.assigned_to_id.present? ? link_to(User.find(tarefa.assigned_to_id).name, user_path(tarefa.assigned_to_id)) : "Não atribuído"  
+    version_name = tarefa.fixed_version ? link_to(tarefa.fixed_version.name, version_path(tarefa.fixed_version)) : "-"  
+    link_tarefa = link_to_issue(tarefa)  
+  
+    if tarefa.id == tarefa_atual_id  
+      link_tarefa = "<strong>#{link_tarefa}</strong>"  
+    end  
+  
+    "<tr>  
+      <td class='subject'>#{numero_sequencial}. #{tarefa.project.name} - #{link_tarefa}</td>  
+      <td class='status'>#{tarefa.status.name}</td>  
+      <td class='assigned_to'>#{assigned_to_name}</td>  
+      <td class='data_criacao'>#{data_criacao}</td>  
+      <td class='data_em_andamento'>#{data_em_andamento || ''}</td>  
+      <td class='data_resolvida'>#{data_resolvida || ''}</td>  
+      <td class='data_fechada'>#{data_fechada || ''}</td>  
+      <td class='version'>#{version_name}</td>  
+      <td class='spent_hours'>#{horas_gastas}h</td>  
+    </tr>"  
   end
 end
