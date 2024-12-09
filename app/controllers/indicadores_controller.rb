91b73381c9df97a682ec0357c631c316c24de4b4
Maglan Diemer <maglancd@gmail.com>
2024-12-09 17:13:54 -0300

    .

--- app/controllers/indicadores_controller.rb
+++ app/controllers/indicadores_controller.rb
@@ -14,9 +14,18 @@ class IndicadoresController < ApplicationController
     # Filtrar as tarefas do projeto criadas no último mês
     issues = @project.issues.where("created_on >= ?", start_date)
 
-    # Continuar com seu agrupamento e contagem
-    @tarefas_por_tipo = issues.group(:tracker_id).count
-    @tarefas_por_status = issues.group(:status_id).count
+    # Agrupar e contar as tarefas por tipo (tracker)
+    @tarefas_por_tipo = issues.group(:tracker_id).count.transform_keys do |tracker_id|
+      Tracker.find(tracker_id).name
+    end
+
+    # Agrupar e contar as tarefas por status
+    @tarefas_por_status = issues.group(:status_id).count.transform_keys do |status_id|
+      IssueStatus.find(status_id).name
+    end
+
+    Rails.logger.info @tarefas_por_tipo
+    Rails.logger.info @tarefas_por_status
 
     @tarefas_por_tipo = {
       "Bug" => 5,
