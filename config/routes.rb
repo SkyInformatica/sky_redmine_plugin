feef72462cd15ec973b76d9203a504632ab85575
Maglan Diemer <maglancd@gmail.com>
2024-12-09 16:19:15 -0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,6 @@ Rails.application.routes.draw do
     get "sky_redmine_settings", to: "sky_redmine_settings#show", as: "sky_redmine_settings"
     post "sky_redmine_settings", to: "sky_redmine_settings#update", as: "update_sky_redmine_settings"
 
-    scope "/projects/:project_id" do
-      get "indicadores", to: "indicadores#index", as: "project_indicadores"
-    end
+    match "projects/:id/indicadores", :to => "indicadores#index", :via => "get"
   end
 end
