fc3d3decbf4e8ae87f2e651eb8f9258eb536acaf
Maglan Diemer <maglancd@gmail.com>
2024-12-09 08:43:38 -0300

    .

--- app/helpers/fluxo_tarefas_helper.rb
+++ app/helpers/fluxo_tarefas_helper.rb
@@ -264,8 +264,8 @@ module FluxoTarefasHelper
 
     if revisoes.any?
       links_revisoes = revisoes.map do |revisao|
-        #link_to("r#{revisao.revision}", controller: "repositories", action: "revision", project_id: tarefa.project.id, rev: revisao.revision)
-        link_to("r#{revisao.revision}", revision_path(project_id: tarefa.project.identifier, rev: revisao.revision))
+        link_to("r#{revisao.revision}", controller: "repositories", action: "revision", project_id: tarefa.project.id, rev: revisao.revision)
+        #link_to("r#{revisao.revision}", revision_path(project_id: tarefa.project.identifier, rev: revisao.revision))
       end.join(", ")
     else
       links_revisoes = "-"
