4930b28c45cc189efcb4df11f47ca8c583848ad0
Maglan Diemer <maglancd@gmail.com>
2024-12-12 11:20:19 -0300

    incluido opcao de filtro por data nos indicadores

--- app/controllers/indicadores_controller.rb
+++ app/controllers/indicadores_controller.rb
@@ -3,15 +3,28 @@ class IndicadoresController < ApplicationController
   before_action :find_project, :authorize
   menu_item :indicadores
 
-  #helper Chartkick::Helper
-
   def index
-    Rails.logger.info ">>>> index indicadores"
-    # Definir a data de início para um mês atrás
-    start_date = 12.month.ago
+    # Determinar o período com base no parâmetro recebido
+    case params[:periodo]
+    when "current_month"
+      start_date = Date.current.beginning_of_month
+      end_date = Date.current.end_of_month
+    when "last_month"
+      start_date = (Date.current - 1.month).beginning_of_month
+      end_date = (Date.current - 1.month).end_of_month
+    when "current_year"
+      start_date = Date.current.beginning_of_year
+      end_date = Date.current.end_of_year
+    else
+      start_date = nil
+      end_date = nil
+    end
 
-    # Filtrar as tarefas do projeto criadas no último mês
-    issues = @project.issues.where("created_on >= ?", start_date)
+    # Filtrar as tarefas com base no período selecionado
+    issues = @project.issues
+    if start_date && end_date
+      issues = issues.where(created_on: start_date..end_date)
+    end
 
     # Agrupar e contar as tarefas por tipo (tracker)
     @tarefas_por_tipo = issues.group(:tracker_id).count.transform_keys do |tracker_id|
@@ -22,9 +35,6 @@ class IndicadoresController < ApplicationController
     @tarefas_por_status = issues.group(:status_id).count.transform_keys do |status_id|
       IssueStatus.find(status_id).name
     end
-
-    Rails.logger.info @tarefas_por_tipo
-    Rails.logger.info @tarefas_por_status
   end
 
   private
