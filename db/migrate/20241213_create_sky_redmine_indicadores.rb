fc89c731ccba667256deca35650486946724d126
Maglan Diemer <maglancd@gmail.com>
2024-12-13 14:32:37 -0300

    nova tabela SkyRedmineIndicadores

---
+++ db/migrate/20241213_create_sky_redmine_indicadores.rb
@@ -0,0 +1,40 @@
+class CreateSkyRedmineIndicadores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sky_redmine_indicadores do |t|
+      t.integer :primeira_tarefa_devel_id
+      t.integer :ultima_tarefa_devel_id
+      t.string :status_ultima_tarefa_devel
+      t.integer :prioridade_primeira_tarefa_devel
+      t.integer :sprint_primeira_tarefa_devel_id
+      t.integer :sprint_ultima_tarefa_devel_id
+      t.integer :projeto_primeira_tarefa_devel_id
+      t.float :tempo_estimado_devel
+      t.float :tempo_gasto_devel
+      t.string :origem_primeira_tarefa_devel
+      t.string :skynet_primeira_tarefa_devel
+      t.string :local_tarefa
+      t.integer :qtd_retorno_testes
+      t.date :data_atendimento_primeira_tarefa_devel
+      t.date :data_andamento_primeira_tarefa_devel
+      t.date :data_resolvida_ultima_tarefa_devel
+      t.date :data_fechamento_ultima_tarefa_devel
+      t.integer :primeira_tarefa_qs_id
+      t.integer :ultima_tarefa_qs_id
+      t.integer :sprint_primeira_tarefa_qs_id
+      t.integer :sprint_ultima_tarefa_qs_id
+      t.integer :projeto_primeira_tarefa_qs_id
+      t.float :tempo_estimado_qs
+      t.float :tempo_gasto_qs
+      t.string :status_ultima_tarefa_qs
+      t.boolean :houve_teste_nok
+      t.date :data_criacao_primeira_tarefa_qs
+      t.date :data_andamento_primeira_tarefa_qs
+      t.date :data_resolvida_ultima_tarefa_qs
+      t.date :data_fechamento_ultima_tarefa_qs
+
+      t.timestamps
+    end
+
+    add_index :sky_redmine_indicadores, :primeira_tarefa_devel_id, unique: true
+  end
+end
