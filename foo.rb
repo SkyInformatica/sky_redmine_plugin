389de850a97af62f8f631f78a537082f842fb04b
Maglan Diemer <maglancd@gmail.com>
2025-01-31 17:23:41 -0300

    Atualiza o carregamento do ambiente do Redmine para usar a constante redmine e melhora a saída do ID da tarefa

--- foo.rb
+++ foo.rb
@@ -1,6 +1,6 @@
 
 # Carrega o ambiente do Redmine
-require File.expand_path('../../../config/environment', __FILE__) # Ajuste o caminho conforme a instalação Redmine  
+require redmine
 
 
 # Define um método para executar o teste
@@ -8,7 +8,7 @@ def foo(issue_id)
   # Busca a tarefa pelo ID
   issue = Issue.find_by(id: issue_id)
 
-  puts issue.id
+  puts "id: #{issue.id}"
 end
 
 # Chame o método de teste junto ao ID da tarefa
