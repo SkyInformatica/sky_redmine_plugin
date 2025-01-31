2221a3b5e284249082c302017dcd23edc6be6433
Maglan Diemer <maglancd@gmail.com>
2025-01-31 17:19:00 -0300

    Remove dependências não utilizadas no arquivo foo.rb

--- foo.rb
+++ foo.rb
@@ -2,11 +2,6 @@
 # Carrega o ambiente do Redmine
 require File.expand_path('../../../config/environment', __FILE__) # Ajuste o caminho conforme a instalação Redmine  
 
-require_relative "app/helpers/fluxo_tarefas_helper"
-require_relative "lib/sky_redmine_plugin/issue_helper_patch"
-require_relative "app/models/sky_redmine_indicadores"
-
-include SkyRedminePlugin::Hooks::ControllerHooks
 
 # Define um método para executar o teste
 def foo(issue_id)
