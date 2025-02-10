1c3e556f17327ff57df832fc0cefd5ccde9db286
Maglan Diemer <maglancd@gmail.com>
2025-02-10 17:56:03 -0300

    Adiciona logging e corrige a busca de tarefas no processamento em lote de indicadores

--- app/controllers/processar_indicadores_controller.rb
+++ app/controllers/processar_indicadores_controller.rb
@@ -1,11 +1,12 @@
 class ProcessarIndicadoresController < ApplicationController
-  unloadable
+  before_action :find_issues, only: [:processar_indicadores_lote]
 
   def processar_indicadores_lote
-    issue_ids = params[:issue_ids]
-    issues = Issue.where(id: issue_ids)
+    Rails.logger.info ">>> processar_indicadores_lote"
+    @issue_ids = params[:ids]
 
-    issues.each do |issue|
+    @issues.each do |issue|
+      Rails.logger.info ">>> processando tarefa #{issue.id}"
       SkyRedminePlugin::Indicadores.processar_indicadores(issue)
     end
 
