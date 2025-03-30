722cc56667aefeb07d5efac08487282abc37818f
Maglan Diemer <maglancd@gmail.com>
2025-03-30 10:11:31 -0300

    Refatora a classe ModelHook no plugin SkyRedminePlugin, reorganizando o código e mantendo a lógica de processamento de exclusões de indicadores. Adiciona logs para melhor acompanhamento durante a configuração do hook.

--- lib/sky_redmine_plugin/hooks/model_hook.rb
+++ lib/sky_redmine_plugin/hooks/model_hook.rb
@@ -1,21 +1,23 @@
 module SkyRedminePlugin
-  class ModelHook < Redmine::Hook::ViewListener
-    def after_plugins_loaded(context = {})
-      Rails.logger.info ">>> Iniciando configuração do Sky Redmine Plugin via Hook"
-      
-      # Adicionar o callback after_destroy ao modelo Issue
-      Issue.class_eval do
-        after_destroy :processar_exclusao_indicador
+  module Hooks
+    class ModelHook < Redmine::Hook::ViewListener
+      def after_plugins_loaded(context = {})
+        Rails.logger.info ">>> Iniciando configuração do Sky Redmine Plugin via Hook"
         
-        private
-        
-        def processar_exclusao_indicador
-          Rails.logger.info ">>> processando exclusão da tarefa #{id}"
-          SkyRedminePlugin::Indicadores.processar_indicadores(self, true)
+        # Adicionar o callback after_destroy ao modelo Issue
+        Issue.class_eval do
+          after_destroy :processar_exclusao_indicador
+          
+          private
+          
+          def processar_exclusao_indicador
+            Rails.logger.info ">>> processando exclusão da tarefa #{id}"
+            SkyRedminePlugin::Indicadores.processar_indicadores(self, true)
+          end
         end
+        
+        Rails.logger.info ">>> Hook configurado com sucesso"
       end
-      
-      Rails.logger.info ">>> Hook configurado com sucesso"
     end
   end
 end 
