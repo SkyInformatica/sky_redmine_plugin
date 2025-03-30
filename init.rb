b0908def5eacc8e4e83be353a1fc71131e10d18e
Maglan Diemer <maglancd@gmail.com>
2025-03-30 09:17:10 -0300

    Renomeia o módulo de patch de Issue para `sky_issue_patch` no plugin SkyRedminePlugin, melhorando a clareza e a organização do código.

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ require "groupdate"
 require_relative "app/helpers/fluxo_tarefas_helper"
 require_relative "lib/sky_redmine_plugin/issue_helper_patch"
 require_relative "app/models/sky_redmine_indicadores"
-require_relative "lib/sky_redmine_plugin/patches/issue_patch"
+require_relative "lib/sky_redmine_plugin/patches/sky_issue_patch"
 
 Redmine::Plugin.register :sky_redmine_plugin do
   name "Sky Redmine plugin"
