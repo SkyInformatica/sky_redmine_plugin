9e0920257c6ce3ae54c060e424af7341a8ea7ace
Maglan Diemer <maglancd@gmail.com>
2025-04-04 14:20:36 -0300

    Remove funcionalidades de progresso e código relacionado ao processamento de indicadores a partir de 2024

--- app/controllers/processar_indicadores_controller.rb
+++ app/controllers/processar_indicadores_controller.rb
@@ -1,6 +1,4 @@
 class ProcessarIndicadoresController < ApplicationController
-  @@progresso_indicadores = { total: 0, processados: 0 }
-
   before_action :find_issues, only: [:processar_indicadores_lote]
   before_action :find_issue, only: [:processar_indicadores_tarefa]
 
@@ -34,13 +32,15 @@ class ProcessarIndicadoresController < ApplicationController
 
   def processar_indicadores_2024
     projetos = SkyRedminePlugin::Constants::Projects::REGISTRAL_PROJECTS + SkyRedminePlugin::Constants::Projects::NOTARIAL_PROJECTS
-    ProcessarIndicadoresJob.perform_later(projetos, 2024)
+    issues = Issue.where(project_id: Project.where(name: projetos).pluck(:id))
+                  .where("created_on >= ?", Date.new(2024, 1, 1))
 
-    flash[:notice] = "O processamento foi iniciado. Você pode acompanhar o progresso."
+    issues.each do |issue|
+      Rails.logger.info ">>> processando tarefa #{issue.id}"
+      SkyRedminePlugin::Indicadores.processar_indicadores(issue)
+    end
+
+    flash[:notice] = "Indicadores das tarefas criadas a partir de 2024 foram processados com sucesso."
     redirect_to sky_redmine_settings
-  end
-
-  def self.progresso_indicadores
-    @@progresso_indicadores
   end
 end
