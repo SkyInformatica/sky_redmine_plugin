073c1839c7a162311c05c65725d561b2f641c0e1
Maglan Diemer <maglancd@gmail.com>
2025-04-06 20:25:54 -0300

    Refatora a limpeza de tags SkyRP_ no controlador de indicadores, alterando a lógica para remover as tags diretamente das tarefas em vez de filtrá-las. A nova abordagem busca todas as tags com o prefixo SkyRP_ e as remove de forma mais eficiente, simplificando o processo e melhorando a legibilidade do código. Mensagens de log foram adicionadas para registrar o sucesso ou falha na remoção das tags.

--- app/controllers/processar_indicadores_controller.rb
+++ app/controllers/processar_indicadores_controller.rb
@@ -47,45 +47,26 @@ class ProcessarIndicadoresController < ApplicationController
   def limpar_tags_2024
     Rails.logger.info ">>> Iniciando limpeza de tags SkyRP_ a partir de 2024"
     
-    # Buscar todos os projetos relevantes
-    projetos = SkyRedminePlugin::Constants::Projects::REGISTRAL_PROJECTS + 
-               SkyRedminePlugin::Constants::Projects::NOTARIAL_PROJECTS +
-               SkyRedminePlugin::Constants::Projects::QS_PROJECTS
-
-    # Buscar todas as tarefas dos projetos a partir de 2024
-    issues = Issue.where(project_id: Project.where(name: projetos).pluck(:id))
-                  .where("created_on >= ?", Date.new(2024, 1, 1))
-
-    total_issues = issues.count
-    Rails.logger.info ">>> Total de tarefas encontradas: #{total_issues}"
-
-    issues.each do |issue|
-      begin
-        # Verificar se a tarefa tem tags
-        if issue.respond_to?(:tag_list)
-          tags_atuais = issue.tag_list.dup
-          
-          # Filtrar apenas as tags que começam com SkyRP_
-          tags_skyrp = tags_atuais.select { |tag| tag.start_with?("SkyRP_") }
-          
-          if tags_skyrp.any?
-            # Remover as tags SkyRP_
-            tags_filtradas = tags_atuais.reject { |tag| tag.start_with?("SkyRP_") }
-            
-            # Atualizar as tags da tarefa
-            issue.tag_list = tags_filtradas
-            issue.save(validate: false)
-            
-            Rails.logger.info ">>> Tarefa #{issue.id}: removidas tags #{tags_skyrp.join(', ')}"
-          end
+    # Buscar todas as tags que começam com SkyRP_
+    tags_skyrp = ActsAsTaggableOn::Tag.where("name LIKE ?", "SkyRP_%")
+    
+    if tags_skyrp.any?
+      Rails.logger.info ">>> Encontradas #{tags_skyrp.count} tags SkyRP_ para remover"
+      
+      # Remover as tags de todas as tarefas
+      tags_skyrp.each do |tag|
+        begin
+          # Remover a tag de todas as tarefas que a possuem
+          tag.taggings.destroy_all
+          Rails.logger.info ">>> Tag #{tag.name} removida com sucesso"
+        rescue => e
+          Rails.logger.error ">>> Erro ao remover tag #{tag.name}: #{e.message}"
+          Rails.logger.error e.backtrace.join("\n")
         end
-      rescue => e
-        Rails.logger.error ">>> Erro ao processar tarefa #{issue.id}: #{e.message}"
-        Rails.logger.error e.backtrace.join("\n")
       end
     end
 
-    flash[:notice] = "Tags SkyRP_ das tarefas criadas a partir de 2024 foram removidas com sucesso."
+    flash[:notice] = "Tags SkyRP_ foram removidas com sucesso."
     redirect_to sky_redmine_settings_path
   end
 end
