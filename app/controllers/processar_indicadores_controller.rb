40863f405d3b5992f85ff3a54299448088e43683
Maglan Diemer <maglancd@gmail.com>
2025-04-06 20:17:20 -0300

    Implementa a limpeza de tags SkyRP_ diretamente no controlador de indicadores, removendo a dependência da tarefa Rake. A nova lógica busca e processa as tarefas criadas a partir de 2024, garantindo a remoção adequada das tags e registrando informações relevantes no log. Essa mudança visa simplificar o processo de manutenção das tags no sistema.

--- app/controllers/processar_indicadores_controller.rb
+++ app/controllers/processar_indicadores_controller.rb
@@ -46,7 +46,46 @@ class ProcessarIndicadoresController < ApplicationController
 
   def limpar_tags_2024
     Rails.logger.info ">>> Iniciando limpeza de tags SkyRP_ a partir de 2024"
-    Rake::Task['sky_redmine_plugin:limpar_tags'].invoke
-    redirect_to sky_redmine_settings_path, notice: "Limpeza de tags SkyRP_ a partir de 2024 concluída"
+    
+    # Buscar todos os projetos relevantes
+    projetos = SkyRedminePlugin::Constants::Projects::REGISTRAL_PROJECTS + 
+               SkyRedminePlugin::Constants::Projects::NOTARIAL_PROJECTS +
+               SkyRedminePlugin::Constants::Projects::QS_PROJECTS
+
+    # Buscar todas as tarefas dos projetos a partir de 2024
+    issues = Issue.where(project_id: Project.where(name: projetos).pluck(:id))
+                  .where("created_on >= ?", Date.new(2024, 1, 1))
+
+    total_issues = issues.count
+    Rails.logger.info ">>> Total de tarefas encontradas: #{total_issues}"
+
+    issues.each do |issue|
+      begin
+        # Verificar se a tarefa tem tags
+        if issue.respond_to?(:tag_list)
+          tags_atuais = issue.tag_list.dup
+          
+          # Filtrar apenas as tags que começam com SkyRP_
+          tags_skyrp = tags_atuais.select { |tag| tag.start_with?("SkyRP_") }
+          
+          if tags_skyrp.any?
+            # Remover as tags SkyRP_
+            tags_filtradas = tags_atuais.reject { |tag| tag.start_with?("SkyRP_") }
+            
+            # Atualizar as tags da tarefa
+            issue.tag_list = tags_filtradas
+            issue.save(validate: false)
+            
+            Rails.logger.info ">>> Tarefa #{issue.id}: removidas tags #{tags_skyrp.join(', ')}"
+          end
+        end
+      rescue => e
+        Rails.logger.error ">>> Erro ao processar tarefa #{issue.id}: #{e.message}"
+        Rails.logger.error e.backtrace.join("\n")
+      end
+    end
+
+    flash[:notice] = "Tags SkyRP_ das tarefas criadas a partir de 2024 foram removidas com sucesso."
+    redirect_to sky_redmine_settings_path
   end
 end
