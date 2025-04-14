d5fecfd798a432f460784c9a6edc657f4b073c82
Maglan Diemer <maglancd@gmail.com>
2025-04-14 09:01:34 -0300

    Atualiza o arquivo `test_helper.rb` para configurar o ambiente de teste utilizando o banco de dados de produção. Desabilita transações durante os testes e implementa a limpeza dos dados após cada teste, garantindo a integridade do ambiente de testes.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,24 @@
 # Load the Redmine helper
 require_relative '../../../test/test_helper'
+
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+# Configuração para usar o banco de dados de produção nos testes
+ActiveRecord::Base.connection.execute("SET FOREIGN_KEY_CHECKS=0")
+ActiveRecord::Base.connection.execute("SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO'")
+
+# Desabilitar transações para evitar problemas com o banco de produção
+class ActiveSupport::TestCase
+  self.use_transactional_tests = false
+  
+  # Limpar dados após cada teste
+  def teardown
+    super
+    SkyRedmineIndicadores.delete_all
+  end
+end
+
+# Configurar o ambiente de teste para usar o banco de produção
+ENV['RAILS_ENV'] = 'production'
+ENV['RAILS_TEST_DB'] = 'redmine'
