4fa94dbdb8e7244cce3f4c2f03df8b99cc3e934d
Maglan Diemer <maglancd@gmail.com>
2025-04-29 12:21:13 -0300

    Corrige o cálculo de dias da data de situação atual para considerar corretamente o tipo de dados datetime.

--- app/helpers/fluxo_tarefas_helper.rb
+++ app/helpers/fluxo_tarefas_helper.rb
@@ -448,7 +448,7 @@ module FluxoTarefasHelper
                     method: :post)
     html << ")"
     if !indicadores.nil? && !indicadores.data_situacao_atual.nil?
-      numero_dias_data_situacao_atual = indicadores.data_situacao_atual ? (Date.today - indicadores.data_situacao_atual).to_i : 0
+      numero_dias_data_situacao_atual = indicadores.data_situacao_atual ? (Date.today - indicadores.data_situacao_atual&.to_date).to_i : 0
       situacao_atual_detalhes = indicadores.data_situacao_atual ? "#{indicadores.situacao_atual} em #{indicadores.data_situacao_atual&.strftime("%d/%m/%Y")} (#{numero_dias_data_situacao_atual} dias)" : "#{indicadores.situacao_atual}"
       html << " #{situacao_atual_detalhes}"
     end
