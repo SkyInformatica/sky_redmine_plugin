6bf379efd986597d0061ceca3cbf4cfdf2af32ea
Maglan Diemer <maglancd@gmail.com>
2025-05-09 14:49:28 -0300

    move configurações do Metabase para o controlador indicadores_v2 e remove inicializador

--- app/controllers/indicadores_v2_controller.rb
+++ app/controllers/indicadores_v2_controller.rb
@@ -4,6 +4,9 @@ class IndicadoresV2Controller < ApplicationController
   before_action :authorize
   menu_item :indicadores_v2
 
+  METABASE_SITE_URL = "https://metabase.skyinformatica.com"
+  METABASE_SECRET_KEY = "0ea69615468a2ff4859d94bb587f94850e8bf2277c2c0b7bb502a7070bbfb337"
+
   def index
     payload = {
       :resource => { :dashboard => 4 },
@@ -11,8 +14,8 @@ class IndicadoresV2Controller < ApplicationController
       :exp => Time.now.to_i + (60 * 10), # 10 minute expiration
     }
 
-    token = JWT.encode(payload, SkyRedminePlugin::MetabaseConfig::SECRET_KEY)
-    @iframe_url = "#{SkyRedminePlugin::MetabaseConfig::SITE_URL}/embed/dashboard/#{token}#bordered=true&titled=true"
+    token = JWT.encode(payload, METABASE_SECRET_KEY)
+    @iframe_url = "#{METABASE_SITE_URL}/embed/dashboard/#{token}#bordered=true&titled=true"
   end
 
   private
--- config/initializers/metabase.rb
+++
@@ -1,6 +0,0 @@
-module SkyRedminePlugin
-  class MetabaseConfig
-    SITE_URL = "https://metabase.skyinformatica.com"
-    SECRET_KEY = "0ea69615468a2ff4859d94bb587f94850e8bf2277c2c0b7bb502a7070bbfb337"
-  end
-end
