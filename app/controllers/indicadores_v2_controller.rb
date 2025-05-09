ebae7033783644eca4b5cad18039c590c2475c82
Maglan Diemer <maglancd@gmail.com>
2025-05-09 15:59:50 -0300

    adiciona logs no método index do IndicadoresV2Controller

--- app/controllers/indicadores_v2_controller.rb
+++ app/controllers/indicadores_v2_controller.rb
@@ -8,6 +8,7 @@ class IndicadoresV2Controller < ApplicationController
   METABASE_SECRET_KEY = "0ea69615468a2ff4859d94bb587f94850e8bf2277c2c0b7bb502a7070bbfb337"
 
   def index
+    Rails.logger.info ">>> IndicadoresV2Controller index"
     payload = {
       :resource => { :dashboard => 4 },
       :params => {},
@@ -16,6 +17,7 @@ class IndicadoresV2Controller < ApplicationController
 
     token = JWT.encode(payload, METABASE_SECRET_KEY)
     @iframe_url = "#{METABASE_SITE_URL}/embed/dashboard/#{token}#bordered=true&titled=false?projeto=#{@project}"
+    Rails.logger.info "#{@iframe_url}"
   end
 
   private
